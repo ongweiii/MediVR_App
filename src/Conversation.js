482e4764fd4efa6c2ce60c7d5369ba26d1afa886
Kevin <kevinchanxy@gmail.com>
2021-01-12 00:56:11 +0800

    created sessions

--- src/Conversation.js
+++ src/Conversation.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import moment from "moment";
+
 export default function Conversation() {
   let { id } = useParams();
   const [chat, setChat] = useState([]);
@@ -18,35 +20,50 @@ export default function Conversation() {
       <h5>Conversation for {id}</h5>
       {loaded ? (
         chat.map((item, index) => (
-          <div
-            className={`d-flex flex-row justify-content-${
-              item.event === "bot" ? "start" : "end"
-            } align-items-center`}
-          >
-            <div
-              style={{
-                maxWidth: "75%"
-              }}
-              className={`text-${item.event === "bot" ? "left" : "right"}`}
-            >
-              <div
-                className="bg-light p-2"
-                style={{
-                  borderTopLeftRadius: 12,
-                  borderTopRightRadius: 12,
-                  borderBottomLeftRadius: item.event === "bot" ? 0 : 12,
-                  borderBottomRightRadius: item.event === "bot" ? 12 : 0
-                }}
-              >
-                <span className="h6">{item.event}</span>
-                <br />
-                <span className="text-end">{item.text}</span>
-                <br />
-              </div>
+          <div key={index}>
+            <div className="text-center">
+              <span className="badge px-2 text-white rounded-pill bg-secondary">
+                Session #{index + 1}:{" "}
+                {moment(item.timestamp * 1000).format("Do MMM YYYY, h:mma")}
+              </span>
+            </div>
+            <div className="px-4 py-2">
+              {item.data.map((item2, index2) => (
+                <div
+                  key={index2}
+                  className={`d-flex flex-row justify-content-${
+                    item2.event === "bot" ? "start" : "end"
+                  } align-items-center `}
+                >
+                  <div
+                    style={{
+                      maxWidth: "75%"
+                    }}
+                    className={`text-${
+                      item2.event === "bot" ? "left" : "right"
+                    }`}
+                  >
+                    <div
+                      className="bg-light p-2"
+                      style={{
+                        borderTopLeftRadius: 12,
+                        borderTopRightRadius: 12,
+                        borderBottomLeftRadius: item2.event === "bot" ? 0 : 12,
+                        borderBottomRightRadius: item2.event === "bot" ? 12 : 0
+                      }}
+                    >
+                      <span className="h6">{item2.event}</span>
+                      <br />
+                      <span className="text-end">{item2.text}</span>
+                      <br />
+                    </div>
 
-              <small className="text-muted">
-                {new Date(item.timestamp * 1000).toLocaleString()}
-              </small>
+                    <small className="text-muted">
+                      {moment(item2.timestamp * 1000).format("h:mma")}
+                    </small>
+                  </div>
+                </div>
+              ))}
             </div>
           </div>
         ))
--- src/Senders.js
+++ src/Senders.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import moment from "moment";
+
 export default function Senders() {
   const [list, setList] = useState([]);
   const [loaded, setLoaded] = useState(false);
@@ -16,11 +18,14 @@ export default function Senders() {
       <h1 className="text-center p-4">MediVR Core</h1>
       {loaded ? (
         list.map((item, index) => (
-          <div className="row bg-light p-2 m-2 rounded align-items-center">
+          <div
+            key={index}
+            className="row bg-light p-2 m-2 rounded align-items-center"
+          >
             <div className="col">
               <h5 className="text-truncate">{item.sender_id}</h5>
               <small className="text-muted">
-                {new Date(item.last * 1000).toLocaleString()}
+                {moment(item.last * 1000).format("Do MMM YYYY, h:mma")}
               </small>
             </div>
             <div className="col-md-auto text-center">
